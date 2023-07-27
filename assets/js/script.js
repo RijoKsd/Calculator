aee3d4c510df4e446992fa3d09d8c2c92d4023e0
Rijo Sebastian <rijobdk@gmail.com>
2023-07-27 13:16:36 +0530

    Finished calculator project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,104 @@
-const select = document.querySelector('select');
-const body =  document.querySelector('body');
-function setTheme(e){
-   const theme =  e.target.value;
-   if( theme == 'dark'){
-    body.style.cssText="background-color:  #131313; color:white;"
-   }
-   else{
-    body.style.cssText="background-color: white; color:black;"
-   }
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+
+const numberButtons = document.querySelectorAll("[data-number");
+const operationButtons = document.querySelectorAll("[data-operation");
+const equalBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const clearBtn = document.querySelector("[data-clear]");
+let previousText = document.querySelector("[data-previous]");
+let currentText = document.querySelector("[data-current]");
+let dotBtn = document.querySelector("[data-dot]");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (currentValue.includes(".") && button.innerText === ".") return;
+    currentValue += button.innerText;
+    currentText.innerText = currentValue;
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (currentValue === "") return;
+    if (previousValue !== "") {
+      compute();
+    }
+    operator = button.innerText;
+    previousValue = currentValue;
+    currentText.innerText = "";
+    previousText.innerText = previousValue + operator;
+    currentValue = "";
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  if (currentValue === "" || previousValue === "") return;
+  compute();
+  updateDisplay();
+});
+
+clearBtn.addEventListener("click", () => {
+  clear();
+  updateDisplay();
+});
+
+deleteBtn.addEventListener("click", () => {
+  currentValue = currentValue.toString().slice(0, -1);
+  updateDisplay();
+});
+
+dotBtn.addEventListener("click", () => {
+  if (currentValue.includes(".")) return;
+  currentValue += ".";
+  updateDisplay();
+});
+
+function compute() {
+  let computation;
+  const prev = parseFloat(previousValue);
+  const current = parseFloat(currentValue);
+  if (isNaN(prev) || isNaN(current)) return;
+  switch (operator) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "/":
+      // zero division not possible
+      if (current === 0) {
+        alert("You can't divide by 0!");
+        return;
+      }
+
+      computation = prev / current;
+      break;
+
+    case "%":
+      computation = prev % current;
+
+      break;
+    default:
+      return;
+  }
+  currentValue = computation;
+  operator = "";
+  previousValue = "";
 }
-select.addEventListener('change',setTheme)
+
+function updateDisplay() {
+  currentText.innerText = currentValue;
+  previousText.innerText = previousValue + operator;
+}
+
+function clear() {
+  currentValue = "";
+  previousValue = "";
+  operator = "";
+}
